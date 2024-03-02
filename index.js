8a46ce3d56bb0bbf1238ab730ce9263e49808fa5
Peter BUSS <peter.buss@hispeed.ch>
2024-03-02 15:38:39 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const chatbotConversation = document.getElementById('chatbot-conversation');
+ 
+let conversationStr = '';
+ 
+document.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userInput = document.getElementById('user-input'); 
+    conversationStr += `${userInput.value}`;
+    console.log("Cs", conversationStr);
+    fetchReply();
+    const newSpeechBubble = document.createElement('div');
+    newSpeechBubble.classList.add('speech', 'speech-human');
+    chatbotConversation.appendChild(newSpeechBubble);
+    newSpeechBubble.textContent = userInput.value;
+    userInput.value = '';
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
+}) ;
+
+async function fetchReply(){
+
+    const url = 'https://netlifyserverlessfcts.netlify.app/fetchAI.js' ;
+
+
+    const url_params = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "text/plain"
+                },
+                body: JSON.stringify(conversationStr)
+            };
+    console.log('url_params', url_params);
+    const response = await fetch(url, url_params);
+    const data = await response.json();
+    console.log(data);
+
+
+}
+
+function renderTypewriterText(text) {
+    const newSpeechBubble = document.createElement('div');
+    newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor');
+    chatbotConversation.appendChild(newSpeechBubble);
+    let i = 0;
+    const interval = setInterval(() => {
+        newSpeechBubble.textContent += text.slice(i-1, i);
+        if (text.length === i) {
+            clearInterval(interval);
+            newSpeechBubble.classList.remove('blinking-cursor');
+        }
+        i++;
+        chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
+    }, 50);
+}
