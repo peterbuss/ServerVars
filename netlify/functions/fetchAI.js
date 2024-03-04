a2e2639143dce858a27550d6bbb83066dd83a61b
Peter BUSS <peter.buss@hispeed.ch>
2024-03-04 23:43:06 +0100

    Changes 4

--- netlify/functions/fetchAI.js
+++ netlify/functions/fetchAI.js
@@ -1,13 +1,19 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
+
 const handler = async (event) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({}),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
