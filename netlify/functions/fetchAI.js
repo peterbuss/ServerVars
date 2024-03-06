a05edb3b542f3c82a5ec2f3d4aec6840eb0e1a1b
Peter BUSS <peter.buss@hispeed.ch>
2024-03-06 17:01:28 +0100

    Changes 25

--- netlify/functions/fetchAI.js
+++ netlify/functions/fetchAI.js
@@ -1,9 +1,11 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-import { Configuration, OpenAIApi } from 'openai'
+import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
-})
+});
+
+const openai = new OpenAIApi(configuration);
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
@@ -12,14 +14,14 @@ const handler = async (event) => {
     // davinci:ft-scrimba-2023-03-30-23-10-03
     
     const response = await openai.createCompletion({
-      model: 'davinci-002',
+      model: 'davinci:ft-scrimba-2023-03-30-23-10-03',
       prompt: event.body,
       presence_penalty: 0,
       frequency_penalty: 0.3,
       max_tokens: 100,
       temperature: 0,
       stop: ['\n', '->']
-    })
+    });
 
     
 
@@ -32,16 +34,17 @@ Challenge:
 */
 
 
-    const subject = event.queryStringParameters.name || 'World';
+    //const subject = event.queryStringParameters.name || 'World';
+
     return {
       statusCode: 200,
       body: JSON.stringify({
         reply: response.data
       }),
-    }
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: error.toString() };
   }
 }
 
-module.exports = { handler }
+module.exports = { handler };
