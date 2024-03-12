f799c80df1e79cc7b637fa08b3c566f1bfe79b0b
Peter BUSS <peter.buss@hispeed.ch>
2024-03-12 16:38:56 +0100

    Try 2 Changes 17

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function fetchReply(input){
     const response = await fetch(url, {
         method: 'POST',
         headers: {
-            'content-type': 'application/json',
+            'content-type': 'text/plain',
         },
         body: JSON.stringify(input)
     });
